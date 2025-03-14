96e5b77256df32df7be564f1708eb7d5f7757acc
jin_chang <jin_chang@tri.com.tw>
2025-03-14 14:50:08 +0800

    implement page_rank && bfs and accelerated with openmp.

--- HW3/part2/breadth_first_search/bfs.cpp
+++ HW3/part2/breadth_first_search/bfs.cpp
@@ -27,85 +27,88 @@ void vertex_set_init(vertex_set *list, int count)
 // Take one step of "top-down" BFS.  For each vertex on the frontier,
 // follow all outgoing edges, and add all neighboring vertices to the
 // new_frontier.
-void top_down_step(
-    Graph g,
-    vertex_set *frontier,
-    vertex_set *new_frontier,
-    int *distances)
-{
-    for (int i = 0; i < frontier->count; i++)
-    {
+int top_down_step(Graph g, int *const distances, const int &current_distance) {
+    int new_frontiers_size = 0;
 
-        int node = frontier->vertices[i];
+    #pragma omp parallel for reduction (+ : new_frontiers_size) schedule (dynamic, 1024)
+    for (int vi = 0; vi < g->num_nodes; vi++) {
+        // frontier vertex's distance equals to travel distance
+        if (distances[vi] == current_distance) {
+            const int start_edge = g->outgoing_starts[vi];
+            const int end_edge = (vi == g->num_nodes - 1) ? g->num_edges : g->outgoing_starts[vi + 1];
 
-        int start_edge = g->outgoing_starts[node];
-        int end_edge = (node == g->num_nodes - 1)
-                           ? g->num_edges
-                           : g->outgoing_starts[node + 1];
-
-        // attempt to add all neighbors to the new frontier
-        for (int neighbor = start_edge; neighbor < end_edge; neighbor++)
-        {
-            int outgoing = g->outgoing_edges[neighbor];
-
-            if (distances[outgoing] == NOT_VISITED_MARKER)
-            {
-                distances[outgoing] = distances[node] + 1;
-                int index = new_frontier->count++;
-                new_frontier->vertices[index] = outgoing;
+            for (int edge = start_edge; edge < end_edge; edge++) {
+                const int out_going = g->outgoing_edges[edge];
+                // Use atomic compare-and-swap to safely update distances
+                if (__sync_bool_compare_and_swap(&distances[out_going], 
+                                                NOT_VISITED_MARKER, 
+                                                current_distance + 1)) {
+                    new_frontiers_size += 1;
+                }
             }
         }
     }
+    return new_frontiers_size;
 }
 
 // Implements top-down BFS.
 //
 // Result of execution is that, for each node in the graph, the
 // distance to the root is stored in sol.distances.
-void bfs_top_down(Graph graph, solution *sol)
-{
+void bfs_top_down(Graph graph, solution *sol) {
+    // initialize distances to NOT_VISITED_MARKER
+    memset(sol->distances, NOT_VISITED_MARKER, graph->num_nodes * sizeof(int));
 
-    vertex_set list1;
-    vertex_set list2;
-    vertex_set_init(&list1, graph->num_nodes);
-    vertex_set_init(&list2, graph->num_nodes);
+    // distance to root node;
+    int current_distance = 0;
+    // setup distance of ROOT_NODE_ID
+    sol->distances[ROOT_NODE_ID] = current_distance;
+    int frontier_size = 1;
 
-    vertex_set *frontier = &list1;
-    vertex_set *new_frontier = &list2;
-
-    // initialize all nodes to NOT_VISITED
-    for (int i = 0; i < graph->num_nodes; i++)
-        sol->distances[i] = NOT_VISITED_MARKER;
-
-    // setup frontier with the root node
-    frontier->vertices[frontier->count++] = ROOT_NODE_ID;
-    sol->distances[ROOT_NODE_ID] = 0;
-
-    while (frontier->count != 0)
-    {
-
+    while (frontier_size > 0) {
 #ifdef VERBOSE
         double start_time = CycleTimer::currentSeconds();
+        printf("frontier=%-10d ", frontier_size);
 #endif
 
-        vertex_set_clear(new_frontier);
-
-        top_down_step(graph, frontier, new_frontier, sol->distances);
+        frontier_size = top_down_step(graph, sol->distances, current_distance);
 
 #ifdef VERBOSE
         double end_time = CycleTimer::currentSeconds();
-        printf("frontier=%-10d %.4f sec\n", frontier->count, end_time - start_time);
+        printf("%.4f sec\n", end_time - start_time);
 #endif
 
-        // swap pointers
-        vertex_set *tmp = frontier;
-        frontier = new_frontier;
-        new_frontier = tmp;
+        current_distance += 1;
     }
 }
 
-void bfs_bottom_up(Graph graph, solution *sol)
-{
+int bottom_up_step(Graph g, int *distances, const int &current_distance) {
+    int new_frontiers_size = 0;
+
+    #pragma omp parallel for reduction (+ : new_frontiers_size) schedule (dynamic, 1024)
+    for (int vi = 0; vi < g->num_nodes; vi++) {
+        // search all vertices that haven't been visited
+        if (distances[vi] == NOT_VISITED_MARKER) {
+            // incoming edges to vi
+            const int start_edge = g->incoming_starts[vi];
+            const int end_edge = (vi == g->num_nodes - 1) ? g->num_edges : g->incoming_starts[vi + 1];
+
+            for (int edge = start_edge; edge < end_edge; edge++) {
+                const int in_coming = g->incoming_edges[edge];
+                // check the incoming edge's distance is equal to current distance (bottom-up)
+                if (distances[in_coming] == current_distance) {
+                    // update distance of current vertex vi to (travel distance + 1)
+                    distances[vi] = current_distance + 1;
+                    new_frontiers_size += 1;
+                    break;
+                }
+            }
+        }
+    }
+    return new_frontiers_size;
+}
+
+void bfs_bottom_up(Graph graph, solution *sol) {
     // For PP students:
     //
     // You will need to implement the "bottom up" BFS here as
@@ -117,6 +120,32 @@ void bfs_bottom_up(Graph graph, solution *sol)
     // As was done in the top-down case, you may wish to organize your
     // code by creating subroutine bottom_up_step() that is called in
     // each step of the BFS process.
+
+    // initialize distances to NOT_VISITED_DISTANCE
+    memset(sol->distances, NOT_VISITED_MARKER, graph->num_nodes * sizeof(int));
+
+    // distance to root node;
+    int current_distance = 0;
+    // setup distance of ROOT_NODE_ID
+    sol->distances[ROOT_NODE_ID] = current_distance;
+    int frontier_size = 1;
+
+    while (frontier_size > 0) {
+
+#ifdef VERBOSE
+    double start_time = CycleTimer::currentSeconds();
+    printf("frontier=%-10d ", frontier_size);
+#endif
+
+        frontier_size = bottom_up_step(graph, sol->distances, current_distance);
+
+#ifdef VERBOSE
+    double end_time = CycleTimer::currentSeconds();
+    printf("%.4f sec\n", end_time - start_time);
+#endif
+
+    current_distance += 1;
+    }
 }
 
 void bfs_hybrid(Graph graph, solution *sol)
@@ -125,4 +154,31 @@ void bfs_hybrid(Graph graph, solution *sol)
     //
     // You will need to implement the "hybrid" BFS here as
     // described in the handout.
+
+    memset(sol->distances, NOT_VISITED_MARKER, graph->num_nodes * sizeof(int));
+
+    int current_distance = 0;
+    sol->distances[ROOT_NODE_ID] = current_distance;
+    int frontier_size = 1;
+    const double threshold = graph->num_nodes * 0.25;
+    while (frontier_size > 0) {
+#ifdef VERBOSE
+        double start_time = CycleTimer::currentSeconds();
+        printf("frontier=%-10d ", frontier_size);
+#endif
+
+        if (frontier_size < threshold) {
+            frontier_size = top_down_step(graph, sol->distances, current_distance);
+        }
+        else {
+            frontier_size = bottom_up_step(graph, sol->distances, current_distance);
+        }
+
+#ifdef VERBOSE
+        double end_time = CycleTimer::currentSeconds();
+        printf("%.4f sec\n", end_time - start_time);
+#endif
+
+        current_distance += 1;
+    }
 }
--- HW3/part2/page_rank/page_rank.cpp
+++ HW3/part2/page_rank/page_rank.cpp
@@ -56,4 +56,60 @@ void pageRank(Graph g, double *solution, double damping, double convergence)
      }
 
    */
+  double *score_old = (double *)malloc(numNodes * sizeof(double));
+  double *score_new = (double *)malloc(numNodes * sizeof(double));
+  bool converged = false;
+
+  for (int i = 0; i < numNodes; i++)
+  {
+    score_old[i] = equal_prob;
+  }
+
+  while (!converged)
+  {
+    for (int i = 0; i < numNodes; i++)
+    {
+      score_new[i] = 0.0;
+    }
+
+    double noOutgoing = 0.0;
+    for (int vi = 0; vi < numNodes; vi++) {
+      if (outgoing_size(g, vi) == 0) {
+        noOutgoing += damping * score_old[vi] / numNodes;
+      }
+    }
+    // score_new[vi] = sum over all nodes vj reachable from incoming edges
+    //                          { score_old[vj] / number of edges leaving vj  }
+    // bottleneck
+    #pragma omp parallel for
+    for (int vi = 0; vi < numNodes; vi++)
+    {
+      double sum = 0.0;
+      for (const Vertex *vj = incoming_begin(g, vi); vj != incoming_end(g, vi); vj++)
+      {
+        sum += score_old[*vj] / outgoing_size(g, *vj);
+      }
+      // score_new[vi] = (damping * score_new[vi]) + (1.0-damping) / numNodes;
+      score_new[vi] = (damping * sum) + (1.0 - damping) / numNodes + noOutgoing;
+    }
+    // score_new[vi] += sum over all nodes v in graph with no outgoing edges
+    //                          { damping * score_old[v] / numNodes }
+
+    // compute how much per-node scores have changed
+    double global_diff = 0.0;
+    for (int vi = 0; vi < numNodes; vi++)
+    {
+      global_diff += fabs(score_new[vi] - score_old[vi]);
+    }
+    converged = (global_diff < convergence);
+
+    for (int vi = 0; vi < numNodes; vi++)
+    {
+      score_old[vi] = score_new[vi];
+    }
+  }
+  
+  memcpy(solution, score_old, numNodes * sizeof(double));
+  free(score_old);
+  free(score_new);
 }
